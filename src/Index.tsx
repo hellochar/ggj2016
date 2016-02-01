a28649fc4a1dec8fc30471aaa3dc72007fca8b28
Xiaohan Zhang <hellocharlien@hotmail.com>
2016-02-01 01:37:29 -0800

    add multiple levels; press q/e to switch levels

--- src/Index.tsx
+++ src/Index.tsx
@@ -58,9 +58,30 @@ interface ILevel {
 }
 
 interface IState {
-    level: ILevel;
+    levels: ILevel[];
     textHistory: string[];
 }
+
+function findUserLevel(levels: ILevel[]) {
+    return levels.filter((level) => level.entities.some((entity) => entity.type === EntityType.USER))[0];
+}
+
+function updateUserLevel(state: IState, update: (ILevel) => ILevel) {
+    const userLevel = findUserLevel(state.levels),
+          userLevelIndex = state.levels.indexOf(userLevel);
+
+    const newLevel = update(userLevel);
+
+    return {
+        levels: [
+            ...state.levels.slice(0, userLevelIndex),
+            newLevel,
+            ...state.levels.slice(userLevelIndex + 1)
+        ],
+        textHistory: state.textHistory
+    }
+}
+
 
 class LifeLikeCA {
     public map: Tile[][];
@@ -237,7 +258,7 @@ function generateMap() {
 const initialMap = generateMap();
 giveVision(initialMap, {x: 30, y: 15}, 7);
 const INITIAL_STATE: IState = {
-    level: {
+    levels: [{
         map: initialMap,
         entities: [
             {
@@ -258,6 +279,22 @@ const INITIAL_STATE: IState = {
             }
         ],
     },
+    {
+        map: generateMap(),
+        entities: []
+    },
+    {
+        map: generateMap(),
+        entities: []
+    },
+    {
+        map: generateMap(),
+        entities: []
+    },
+    {
+        map: generateMap(),
+        entities: []
+    }],
     textHistory: [
         "Welcome, hellochar, to the Peregrin Caves! You hear the light trickling of water nearby. The damp moss crunches underneath your feet. The dungeon glows with an eerie light.",
         "Mercury is on this level!"
@@ -294,23 +331,29 @@ function createMoveAction(direction: Position): IMoveAction {
 }
 
 function handleMoveAction(state: IState, action: IMoveAction): IState {
-    const user = state.level.entities[0];
-    const newUser: IEntity = {
-        type: user.type,
-        x: user.x + action.direction.x,
-        y: user.y + action.direction.y,
-        health: user.health,
-        maxHealth: user.maxHealth,
-        name: user.name
-    };
-    const newMap = clone(state.level.map);
-    const discoveryTexts = giveVision(newMap, newUser, 7);
-    return {
-        level: {
+    let discoveryTexts: string[] = [];
+
+    const {levels, textHistory} = updateUserLevel(state, (userLevel) => {
+        const user = userLevel.entities[0];
+        const newUser: IEntity = {
+            type: user.type,
+            x: user.x + action.direction.x,
+            y: user.y + action.direction.y,
+            health: user.health,
+            maxHealth: user.maxHealth,
+            name: user.name
+        };
+        const newMap = clone(userLevel.map);
+        discoveryTexts = giveVision(newMap, newUser, 7);
+        return {
             map: newMap,
-            entities: [newUser, ...state.level.entities.splice(1)]
-        },
-        textHistory: [...state.textHistory, ...discoveryTexts]
+            entities: [newUser, ...userLevel.entities.slice(1)]
+        };
+    });
+
+    return {
+        levels,
+        textHistory: [...textHistory, ...discoveryTexts]
     };
 }
 
@@ -325,15 +368,55 @@ function createMapEvolveAction(ruleset: string): IMapEvolveAction {
 }
 
 function handleMapEvolveAction(state: IState, action: IMapEvolveAction): IState {
-    const ca = new LifeLikeCA(state.level.map, action.ruleset);
-    const newMap = ca.simulate();
-    return {
-        level: {
+    return updateUserLevel(state, (level: ILevel) => {
+        const ca = new LifeLikeCA(level.map, action.ruleset);
+        const newMap = ca.simulate();
+        return {
             map: newMap,
-            entities: state.level.entities
-        },
-        textHistory: state.textHistory
+            entities: level.entities
+        };
+    });
+}
+
+interface IChangeLevelAction extends IAction {
+    newLevel: number;
+}
+function createChangeLevelAction(newLevel: number): IChangeLevelAction {
+    return {
+        newLevel,
+        type: "CHANGE_LEVEL"
     }
+}
+
+function handleChangeLevelAction(state: IState, action: IChangeLevelAction) {
+    // delete entity from old level
+    const newLevelIndex = action.newLevel;
+    let user: IEntity;
+    const {levels, textHistory} = updateUserLevel(state, (level: ILevel) => {
+        user = level.entities[0];
+        return {
+            map: level.map,
+            entities: level.entities.slice(1)
+        };
+    });
+
+    const newMap = levels[newLevelIndex].map;
+    const discoveryTexts = giveVision(newMap, user, 7);
+    const newLevel: ILevel = {
+        map: newMap,
+        entities: [user, ...levels[newLevelIndex].entities.slice(1)]
+    }
+
+    const newLevels = [
+        ...levels.slice(0, newLevelIndex),
+        newLevel,
+        ...levels.slice(newLevelIndex + 1)
+    ];
+
+    return {
+        levels: newLevels,
+        textHistory: [...textHistory, `You have entered floor ${newLevelIndex + 1}.`, ...discoveryTexts]
+    };
 }
 
 function reducer(state: IState = INITIAL_STATE, action: IAction) {
@@ -341,6 +424,8 @@ function reducer(state: IState = INITIAL_STATE, action: IAction) {
         return handleMoveAction(state, action as IMoveAction);
     } else if(action.type === "MAP_EVOLVE") {
         return handleMapEvolveAction(state, action as IMapEvolveAction);
+    } else if(action.type === "CHANGE_LEVEL") {
+        return handleChangeLevelAction(state, action as IChangeLevelAction);
     } else {
         return state;
     }
@@ -349,23 +434,17 @@ function reducer(state: IState = INITIAL_STATE, action: IAction) {
 const store = Redux.createStore(reducer);
 
 
-class EntityInfo extends React.Component<{entity: IEntity}, {}> {
+class PureEntityInfo extends React.Component<{entity: IEntity, floor: number}, {}> {
     render() {
         const {entity} = this.props;
         return <div className="entity-info">
-            <h3>{entity.name}</h3>
+            <h3>{entity.name} (floor {this.props.floor + 1})</h3>
             <p>{entity.health} / {entity.maxHealth}</p>
         </div>;
     }
 }
 
-interface IGameProps extends React.Props<{}> {
-    level?: ILevel;
-    dispatch?: Redux.Dispatch;
-    textHistory?: string[];
-}
-
-class PureGame extends React.Component<IGameProps, {}> {
+class PureLevel extends React.Component<{level: ILevel}, {}> {
     iconClassForTile(tile: TileType) {
         switch(tile) {
             case TileType.SPACE: return 'fa-square-o space';
@@ -380,28 +459,6 @@ class PureGame extends React.Component<IGameProps, {}> {
         } else {
             return <i className="fa tile unexplored"> </i>;
         }
-    }
-
-    onKeyPress(event: any) {
-        const mapping = {
-            KeyW: Direction.UP,
-            KeyA: Direction.LEFT,
-            KeyS: Direction.DOWN,
-            KeyD: Direction.RIGHT
-        };
-        if (event.code === "KeyZ") {
-            this.props.dispatch(createMapEvolveAction("45678/3"));
-        }
-        if (event.code === "KeyX") {
-            this.props.dispatch(createMapEvolveAction("1234/3"));
-        }
-        if (mapping[event.code]) {
-            this.props.dispatch(createMoveAction(mapping[event.code]));
-        }
-    }
-
-    public componentDidMount() {
-        document.addEventListener("keypress", (event) => this.onKeyPress(event), false);
     }
 
     iconClassForEntity(entity: IEntity) {
@@ -420,23 +477,74 @@ class PureGame extends React.Component<IGameProps, {}> {
     }
 
     render() {
+        return <pre className="map">
+            {this.props.level.map.map((row) => {
+                return (
+                    <div className="row">
+                        {row.map((tile) => this.elementForTile(tile))}
+                    </div>
+                );
+            })}
+            {this.props.level.entities.map((entity) => this.elementForEntity(entity))}
+        </pre>;
+    }
+}
+
+interface IGameProps extends React.Props<{}> {
+    dispatch?: Redux.Dispatch;
+
+    levels?: ILevel[];
+    textHistory?: string[];
+}
+
+class PureGame extends React.Component<IGameProps, {}> {
+    onKeyPress(event: any) {
+        const mapping = {
+            KeyW: Direction.UP,
+            KeyA: Direction.LEFT,
+            KeyS: Direction.DOWN,
+            KeyD: Direction.RIGHT
+        };
+        if (event.code === "KeyZ") {
+            this.props.dispatch(createMapEvolveAction("45678/3"));
+        }
+        if (event.code === "KeyX") {
+            this.props.dispatch(createMapEvolveAction("1234/3"));
+        }
+
+        const userLevel = findUserLevel(this.props.levels),
+              userLevelIndex = this.props.levels.indexOf(userLevel);
+        if (event.code === "KeyQ") {
+            this.props.dispatch(createChangeLevelAction(userLevelIndex - 1));
+        }
+        if (event.code === "KeyE") {
+            this.props.dispatch(createChangeLevelAction(userLevelIndex + 1));
+        }
+        if (mapping[event.code]) {
+            this.props.dispatch(createMoveAction(mapping[event.code]));
+        }
+    }
+
+    public componentDidMount() {
+        document.addEventListener("keypress", (event) => this.onKeyPress(event), false);
+    }
+
+    render() {
+        const userLevel = findUserLevel(this.props.levels),
+              userLevelIndex = this.props.levels.indexOf(userLevel);
+
         return (
             <div className="game">
-                <pre className="map">
-                    {this.props.level.map.map((row) => {
-                        return (
-                            <div className="row">
-                                {row.map((tile) => this.elementForTile(tile))}
-                            </div>
-                        );
-                    })}
-                    {this.props.level.entities.map((entity) => this.elementForEntity(entity))}
-                </pre>
+                <PureLevel level={userLevel} />
                 <div className="hud">
-                    <EntityInfo entity={this.props.level.entities[0]} />
+                    <PureEntityInfo entity={userLevel.entities[0]}
+                                    floor={userLevelIndex}/>
                     <ul className="history">
                         {this.props.textHistory.map((text) => <li>{text}</li>)}
                     </ul>
+                    <pre>
+                    {this.props.levels.length}
+                    </pre>
                 </div>
             </div>
         );
