99df41c1e605c5d5045f56f0538f98deeef79631
Xiaohan Zhang <hellocharlien@hotmail.com>
2016-11-06 01:25:03 -0500

    delete unused node.d.ts typings

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="classnames/classnames.d.ts" />
 /// <reference path="jasmine/jasmine.d.ts" />
-/// <reference path="node/node.d.ts" />
 /// <reference path="react/react.d.ts" />
 /// <reference path="react/react-dom.d.ts" />
 /// <reference path="react/react-addons-test-utils.d.ts" />
