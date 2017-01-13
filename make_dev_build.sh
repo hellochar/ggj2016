cd78fada7f0f46f7016f4612ceab70f16131d76a
Xiaohan Zhang <hellocharlien@hotmail.com>
2017-01-13 02:35:03 -0500

    create dev builds using production node_env

--- make_dev_build.sh
+++ make_dev_build.sh
@@ -1,5 +1,5 @@
 # to make a dev build:
 # emit webpack output in a production environment
-npm run build
+NODE_ENV=production npm run build
 cp index_dev_build.html build/index.html
 cp -r font-awesome-4.5.0 build/
